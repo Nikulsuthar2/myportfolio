6c9eec1c1b0fd6b025453cfdc694777a5d02d89b
Nikul Suthar <nikulsuthar973@gmail.com>
2024-12-22 12:04:18 +0530

    resume updated

--- src/components/ProjectsShowCase.jsx
+++ src/components/ProjectsShowCase.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { BiArrowBack } from 'react-icons/bi'
 
 const ProjectsShowCase = () => {
   return (
-    <div>ProjectsShowCase</div>
+    <div className='bg-[#0F0341] p-[10px] flex flex-col text-white cursor-default'>
+        <div className='flex items-center gap-[20px]'>
+            <BiArrowBack onClick={()=>history.back()} size={40}  className='p-[10px] cursor-pointer bg-white font-bold text-black rounded-xl active:translate-x-1'/>
+            <span className='text-[40px] font-bold'>Projects</span>
+        </div>
+        <div id='nikcalculator' className='flex'>
+            <div>
+
+            </div>
+            <div className='flex flex-col'>
+                <h1>Nik Calulator</h1>
+            </div>
+        </div>
+    </div>
   )
 }
 
